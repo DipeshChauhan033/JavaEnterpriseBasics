f7b5c7495549cc7649347198638f55c20ae1bc77
Dipesh <dipeshchauhan9898@gmail.com>
2026-02-12 23:31:04 +0530

    ActionListenerOnButton

---
+++ 01-Basic/ActionListener/ActionOnButton.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends JFrame implements ActionListener{
+    Container c;
+    MyFrame(){
+        c = getContentPane();
+        c.setLayout(null);
+        JButton btn = new JButton("Click Me..");
+        btn.setBounds(190,180,100,30);
+        c.add(btn);
+        btn.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        c.setBackground(Color.BLACK);
+    }
+}
+
+public class ActionOnButton{
+    public static void main(String[] args){
+        MyFrame f = new MyFrame();
+        f.setBounds(500,270,500,450);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+        f.setResizable(false);
+    }
+}
---
+++ 01-Basic/ActionListener/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+btn.addActionListener(this);
