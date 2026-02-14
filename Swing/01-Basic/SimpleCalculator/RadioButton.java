e1b051fcf2c71f95fa8d5273385e86dc73fdbb09
Dipesh <dipeshchauhan9898@gmail.com>
2026-02-15 00:30:33 +0530

    UpdateFile

--- Swing/01-Basic/SimpleCalculator/RadioButton.java
+++ Swing/01-Basic/SimpleCalculator/RadioButton.java
@@ -12,10 +12,41 @@ public class RadioButton{
         JRadioButton rbt1 = new JRadioButton("Male");
         rbt1.setBounds(50,60,100,50);
         c.add(rbt1);
+        rbt1.setSelected(true);  //To preselect
 
         JRadioButton rbt2 = new JRadioButton("Female");
         rbt2.setBounds(50,100,100,50);
         c.add(rbt2);
+
+        ButtonGroup gender =  new ButtonGroup();
+        gender.add(rbt1);
+        gender.add(rbt2);
+
+        JRadioButton general = new JRadioButton("GENERAL");
+        general.setBounds(50,150,100,50);
+        c.add(general);
+        general.setSelected(true);
+        
+        JRadioButton obc = new JRadioButton("OBC");
+        obc.setBounds(150,150,100,50);
+        c.add(obc);
+        
+        JRadioButton sc = new JRadioButton("SC");
+        sc.setBounds(250,150,100,50);
+        c.add(sc);
+    
+        JRadioButton st = new JRadioButton("ST");
+        st.setBounds(350,150,100,50);
+        c.add(st);
+      
+        ButtonGroup cast = new ButtonGroup();
+        cast.add(general);
+        cast.add(obc);
+        cast.add(st);
+        cast.add(sc);
+        
+        //Disable..
+        sc.setEnabled(false);
 
         f.setResizable(false);
         f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
