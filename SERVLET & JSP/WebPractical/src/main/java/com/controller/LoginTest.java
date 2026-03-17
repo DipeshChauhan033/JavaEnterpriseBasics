00a4f59c379a715698d2de916898084aa2a9c472
Dipesh <dipeshchauhan9898@gmail.com>
2026-03-17 22:46:45 +0530

    ProgramOFServlet

---
+++ SERVLET & JSP/WebPractical/src/main/java/com/controller/LoginTest.java
@@ -0,0 +1,18 @@
+package com.controller;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginTest extends HttpServlet {
+
+	 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		System.out.println("Username: "+username+"\n"+"Password: "+password);
+	} 
+
+}
